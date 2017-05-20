a66e16e818342f457464e4c9128cee2cf4fe136c
sarahsweat <sarahsweat@gmail.com>
2017-05-20 14:42:09 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    matches = []
+    anagrams.each do |ind_word|
+      if ind_word.split("").sort == self.word.split("").sort
+        puts "#{@word} is a match with #{ind_word}"
+        matches << ind_word
+      end
+    end
+    matches
+  end
+
+end
